d92d7c4b24e24b57dd980f76859cae0c213427a8
Viviana McIntyre <mcintyvi@oregonstate.edu>
2025-11-10 21:25:29 -0800

    removed unnecessary lines from studyRoute.js

--- backend/routes/studyRoute.js
+++ backend/routes/studyRoute.js
@@ -5,8 +5,4 @@ const studyController = require('../controllers/studyController');
 // Route to create a new study
 router.post('/create', studyController.createStudy);
 
-router.get('/', (req, res) => {
-    res.send('Study route is working!');
-});
-
 module.exports = router;
