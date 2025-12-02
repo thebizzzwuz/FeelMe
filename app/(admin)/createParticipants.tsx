2fa7c0f453532801393480f622253f7e4b11a2f9
Joshua Goldbloom <85968184+goldbloj@users.noreply.github.com>
2025-12-02 17:46:04 -0500

    Update createParticipants.tsx

--- app/(admin)/createParticipants.tsx
+++ app/(admin)/createParticipants.tsx
@@ -27,7 +27,7 @@ export default function CreateParticipants() {
 
     // Scrape database for a list of study names
 
-    axios.get('http://192.168.1.55:3000/api/study/studyname').then((res) => setStudiesList(res.data))
+    axios.get('https://feelme.onrender.com/api/study/studyname').then((res) => setStudiesList(res.data))
         .catch((err) => console.error("Error", err)); }, []);
 
   // Register study participant function to post Participant ID and password to Express backend
@@ -45,7 +45,7 @@ export default function CreateParticipants() {
     }
 
     // The local IP for Expo and the backend server port
-    axios.post('http://192.168.1.55:3000/api/auth/register', {participantId, password, assignedResearcher, researcherEmail, irbApprovalNumber, study})
+    axios.post('https://feelme.onrender.com/api/auth/register', {participantId, password, assignedResearcher, researcherEmail, irbApprovalNumber, study})
         .then(res => {
           alert(res.data.status);
 
